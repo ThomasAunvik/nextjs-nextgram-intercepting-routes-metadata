0d8c2b439e0749eee0ed37a14c3c7423f956904f
Max Mayorov <max.mayorov@boondoggle.eu>
2024-01-16 09:24:17 +0100

    Update dependencies and add async to loading components

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,4 @@
-export default function Home() {
-  return <h1>About</h1>;
+export default async function Home() {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  return <h1>ABOUT</h1>;
 }
--- src/app/about/us/page.tsx
+++ src/app/about/us/page.tsx
@@ -1,3 +1,4 @@
-export default function Home() {
+export default async function Home() {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
   return <h1>ABOUT US</h1>;
 }
